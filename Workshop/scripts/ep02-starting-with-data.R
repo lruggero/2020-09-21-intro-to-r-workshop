b1b9a5655c7814afb715a1e63f9531a4d4dded67
Workshop User <noreply@workshop.com>
2020-09-22 00:07:21 +0000

    Started on Episode 2 - subsetting

--- Workshop/scripts/ep02-starting-with-data.R
+++ Workshop/scripts/ep02-starting-with-data.R
@@ -13,70 +13,114 @@
 
 # Lets download some data (make sure the data folder exists)
 download.file(url = "https://ndownloader.figshare.com/files/2292169",
-              destfile = "data_raw/portal_data_joined.csv")
+              destfile = "data_raw/portal_data_joined.csv") # this tells R to make a copy of this CSV, call it "portal data" and put it in the folder "data raw"
+
+# you can also manually insert a file through "files" in the right hand lower corner then uploading it
+# if you're using R locally on your computer - there's no need to upload because R can just read the files straight off your drive
+# in cloud versions you definitely need to import the file
+
 
 # now we will read this "csv" into an R object called "surveys"
-surveys <- read.csv("data_raw/portal_data_joined.csv")
+surveys <- read.csv("data_raw/portal_data_joined.csv") # this assigns the csv to a new object in R called "Surveys"
+
+#once done it will appear in the environment
 
 # and take a look at it
+surveys
+# in the console pane it will show you the first ~75 rows but then stop, and not bother to show you all 30 000 rows
 
+head(surveys)
+#this just gives a preview of the head of the data (beginning)
+
+tail(surveys)
+#this will show you the last 6 rows
+
+#you can change the number you want to see by adding an argument
+tail(surveys, 2)
+
+View(surveys)
+#this will show you a more traditional spreadsheet view in a new tab
+
+names(surveys)
+#this will show you the first row of the columns / variables IE what are your variables/columns called?
+row.names(surveys)
+#this will sho you the row names (if you have them assigned)
+
+summary(surveys)
+#gives a general summary of your data, by column/variable names (mean, median, quartilces, min, max, no per category of category variables)
 
 
 # BTW, we assumed our data was comma separated, however this might not
-# always be the case. So we may been to tell read.csv more about our file.
+# always be the case. So we may need to tell read.csv more about our file.
 
 
 
 # So what kind of an R object is "surveys" ?
-
-
+# it's not a vector becasue it's got multiple rows and columns, of different types
+# R specifically has an object for coping with spreadsheet type data: "dataframe"
+class(surveys)
+# [1] "data.frame"
 
 # ok - so what are dataframes ?
-
-
+?data.frame
+str(surveys) #this asks for the structure - IE meta info about the spreadsheet (this variable is an integer, this is a 'factor' or categorial variable)
+dim(surveys) #this gives us dimanesions (rows and columns)
+nrow(surveys)
+ncol(surveys)
 
 # --------
 # Exercise
 # --------
 #
 # What is the class of the object surveys?
-#
+    #class(surveys) = data.frame
 # Answer:
 
 
 # How many rows and how many columns are in this survey ?
-#
-# Answer:
+    # dim function for dimensions
+    # nrow for rows (rows and observations used interchangeably in R functions)
+    # ncol for columns (columns and variables used interchangeably in R functions)
+# Answer: 34786 and 13
 
 # What's the average weight of survey animals
-#
-#
-# Answer:
+mean(surveys$weight, na.rm=TRUE)
+# 1 go and figure out what the column is called
+# use the mean function, specify what project, what column and tell it to remove the NA values
+# but you could also use te sumamry function to find this
+# Answer: 42.67243
+
+
 
 # Are there more Birds than Rodents ?
+# could use the summary function to have a look at the means for everything
 #
-#
-# Answer:
+# Answer: No
+# could also use code to find out these two numbers
+sum(surveys$taxa == 'Rodent')
+sum(surveys$taxa == 'Bird')
+
 
 
 # 
 # Topic: Sub-setting
-#
+# need to use square brackets to select a subset of data
 
 # first element in the first column of the data frame (as a vector)
-
+surveys[1,1] # this says tell me row 1, column 1
 
 # first element in the 6th column (as a vector)
+surveys[1,6] # it will give you row 1, column 6, the actual value then it also tells you waht all the possible categorical values are
 
-
-# first column of the data frame (as a vector)
-
+# sixth column of the data frame (as a vector)
+surveys[,6] #you just leave out the row number to tell it to give you the whole column (in this case it will cut out because it's too large to bother)
+surveys[,2]
 
 # first column of the data frame (as a data frame)
 
 
 # first row (as a data frame)
-
+surveys[1,]
 
 # first three elements in the 7th column (as a vector)
 
